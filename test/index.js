c34ff46bc502c206c24a8de6c036d8c63034bf10
Gergo Erdosi <gergo@gergoerdosi.com>
2014-11-29 23:49:31 +0100

    Update code to hapi 8.0.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
 // Load modules
 
-var Lab = require('lab');
+var Code = require('code');
 var Hapi = require('hapi');
+var Lab = require('lab');
 var Oz = require('oz');
 
 
@@ -12,11 +13,10 @@ var internals = {};
 
 // Test shortcuts
 
-var expect = Lab.expect;
-var before = Lab.before;
-var after = Lab.after;
-var describe = Lab.experiment;
-var it = Lab.test;
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
 
 
 describe('Scarecrow', function () {
@@ -69,9 +69,11 @@ describe('Scarecrow', function () {
         };
 
         var server = new Hapi.Server();
-        server.pack.register(require('../'), function (err) {
+        server.connection();
 
-            expect(err).to.not.exist;
+        server.register(require('../'), function (err) {
+
+            expect(err).to.not.exist();
 
             // Add strategy
 
@@ -99,7 +101,7 @@ describe('Scarecrow', function () {
 
                 Oz.ticket.rsvp(apps.social, grant, encryptionPassword, {}, function (err, rsvp) {
 
-                    expect(err).to.not.exist;
+                    expect(err).to.not.exist();
 
                     // After granting app access, the user returns to the app with the rsvp
                     // The app exchanges the rsvp for a ticket
@@ -150,6 +152,110 @@ describe('Scarecrow', function () {
             });
         });
     });
+
+    it('fails to authenticate a request with mismatching app id', function (done) {
+
+        var encryptionPassword = 'password';
+
+        var app = {
+            id: 'social',
+            scope: ['a', 'b', 'c'],
+            key: 'werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn',
+            algorithm: 'sha256'
+        };
+
+        var grant = {
+            id: 'a1b2c3d4e5f6g7h8i9j0',
+            app: 'social',
+            user: 'john',
+            exp: Oz.hawk.utils.now() + 60000
+        };
+
+        var options = {
+            oz: {
+                encryptionPassword: encryptionPassword,
+
+                loadAppFunc: function (id, callback) {
+
+                    callback(null, app);
+                },
+
+                loadGrantFunc: function (id, callback) {
+
+                    callback(null, grant);
+                }
+            }
+        };
+
+        var server = new Hapi.Server();
+        server.connection();
+
+        server.register(require('../'), function (err) {
+
+            expect(err).to.not.exist();
+
+            // Add strategy
+
+            server.auth.strategy('oz', 'oz', true, options);
+
+            // Add a protected resource
+
+            server.route({ path: '/protected', method: 'GET', config: { auth: { entity: 'user' }, handler: function (request, reply) { reply(request.auth.credentials.user + ' your in!'); } } });
+
+            // The app requests an app ticket using Hawk authentication
+
+            var req = {
+                method: 'POST',
+                url: 'http://example.com/oz/app',
+                headers: {
+                    authorization: Oz.client.header('http://example.com/oz/app', 'POST', app).field
+                }
+            };
+
+            server.inject(req, function (res) {
+
+                // The user is redirected to the server, logs in, and grant app access, resulting in an rsvp
+
+                var appTicket = res.result;
+
+                Oz.ticket.rsvp(app, grant, encryptionPassword, {}, function (err, rsvp) {
+
+                    expect(err).to.not.exist();
+
+                    // After granting app access, the user returns to the app with the rsvp
+                    // The app exchanges the rsvp for a ticket
+
+                    var req = {
+                        method: 'POST',
+                        url: 'http://example.com/oz/rsvp',
+                        headers: {
+                            authorization: Oz.client.header('http://example.com/oz/rsvp', 'POST', appTicket).field
+                        },
+                        payload: JSON.stringify({ rsvp: rsvp })
+                    };
+
+                    server.inject(req, function (res) {
+
+                        var userTicket = res.result;
+                        userTicket.app = '567';
+
+                        var req = {
+                            method: 'GET',
+                            url: 'http://example.com/protected',
+                            headers: {
+                                authorization: Oz.client.header('http://example.com/protected', 'GET', userTicket).field
+                            }
+                        };
+
+                        server.inject(req, function (res) {
+
+                            expect(res.statusCode).to.equal(401);
+                            expect(res.result.message).to.equal('Error: Mismatching application id');
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
 });
-
-
