d0c41447d3782908c5219f650cc43e2a680213f8
Eran Hammer <eran@hammer.io>
2017-10-27 11:03:51 -0700

    Partial hapi 17

--- test/index.js
+++ test/index.js
@@ -16,15 +16,13 @@ const internals = {};
 
 // Test shortcuts
 
-const lab = exports.lab = Lab.script();
-const describe = lab.describe;
-const it = lab.it;
+const { describe, it } = exports.lab = Lab.script();
 const expect = Code.expect;
 
 
 describe('Scarecrow', () => {
 
-    it('performs a full authorization flow', (done) => {
+    it('performs a full authorization flow', async () => {
 
         const encryptionPassword = 'a_password_that_is_not_too_short_and_also_not_very_random_but_is_good_enough';
 
@@ -54,121 +52,102 @@ describe('Scarecrow', () => {
             oz: {
                 encryptionPassword,
 
-                loadAppFunc: function (id, callback) {
-
-                    callback(null, apps[id]);
-                },
-
-                loadGrantFunc: function (id, callback) {
+                loadAppFunc: (id) => apps[id],
+                loadGrantFunc: function (id) {
 
                     const ext = {
                         public: 'everybody knows',
                         private: 'the the dice are loaded'
                     };
 
-                    callback(null, grant, ext);
+                    return { grant, ext };
                 }
             }
         };
 
-        const server = new Hapi.Server();
-        server.connection();
+        const server = Hapi.server();
+        await server.register(Scarecrow);
 
-        server.register(Scarecrow, (err) => {
+        // Add strategy
 
-            expect(err).to.not.exist();
+        server.auth.strategy('oz', 'oz', options);
+        server.auth.default('oz');
 
-            // Add strategy
+        // Add a protected resource
 
-            server.auth.strategy('oz', 'oz', true, options);
+        server.route({
+            path: '/protected',
+            method: 'GET',
+            config: {
+                auth: {
+                    entity: 'user'
+                },
+                handler: function (request) {
 
-            // Add a protected resource
-
-            server.route({
-                path: '/protected',
-                method: 'GET',
-                config: {
-                    auth: {
-                        entity: 'user'
-                    },
-                    handler: function (request, reply) {
-
-                        reply(request.auth.credentials.user + ' your in!');
-                    }
+                    return request.auth.credentials.user + ' your in!';
                 }
-            });
-
-            // The app requests an app ticket using Hawk authentication
-
-            let req = {
-                method: 'POST',
-                url: 'http://example.com/oz/app',
-                headers: {
-                    authorization: Oz.client.header('http://example.com/oz/app', 'POST', apps.social).field
-                }
-            };
-
-            server.inject(req, (res1) => {
-
-                // The user is redirected to the server, logs in, and grant app access, resulting in an rsvp
-
-                const appTicket = res1.result;
-
-                Oz.ticket.rsvp(apps.social, grant, encryptionPassword, {}, (err, rsvp) => {
-
-                    expect(err).to.not.exist();
-
-                    // After granting app access, the user returns to the app with the rsvp
-                    // The app exchanges the rsvp for a ticket
-
-                    req = {
-                        method: 'POST',
-                        url: 'http://example.com/oz/rsvp',
-                        headers: {
-                            authorization: Oz.client.header('http://example.com/oz/rsvp', 'POST', appTicket).field
-                        },
-                        payload: JSON.stringify({ rsvp })
-                    };
-
-                    server.inject(req, (res2) => {
-
-                        const userTicket = res2.result;
-
-                        // The app reissues the ticket
-
-                        req = {
-                            method: 'POST',
-                            url: 'http://example.com/oz/reissue',
-                            headers: {
-                                authorization: Oz.client.header('http://example.com/oz/reissue', 'POST', userTicket).field
-                            }
-                        };
-
-                        server.inject(req, (res3) => {
-
-                            const newTicket = res3.result;
-
-                            req = {
-                                method: 'GET',
-                                url: 'http://example.com/protected',
-                                headers: {
-                                    authorization: Oz.client.header('http://example.com/protected', 'GET', newTicket).field
-                                }
-                            };
-
-                            server.inject(req, (res4) => {
-
-                                expect(res4.payload).to.equal('john your in!');
-                                done();
-                            });
-                        });
-                    });
-                });
-            });
+            }
         });
+
+        // The app requests an app ticket using Hawk authentication
+
+        let req = {
+            method: 'POST',
+            url: 'http://example.com/oz/app',
+            headers: {
+                authorization: Oz.client.header('http://example.com/oz/app', 'POST', apps.social).header
+            }
+        };
+
+        const res1 = await server.inject(req);
+        expect(res1.statusCode).to.equal(200);
+
+        // The user is redirected to the server, logs in, and grant app access, resulting in an rsvp
+
+        const appTicket = res1.result;
+        const rsvp = await Oz.ticket.rsvp(apps.social, grant, encryptionPassword);
+
+        // After granting app access, the user returns to the app with the rsvp
+        // The app exchanges the rsvp for a ticket
+
+        req = {
+            method: 'POST',
+            url: 'http://example.com/oz/rsvp',
+            headers: {
+                authorization: Oz.client.header('http://example.com/oz/rsvp', 'POST', appTicket).header
+            },
+            payload: JSON.stringify({ rsvp })
+        };
+
+        const res2 = await server.inject(req);
+        const userTicket = res2.result;
+
+        // The app reissues the ticket
+
+        req = {
+            method: 'POST',
+            url: 'http://example.com/oz/reissue',
+            headers: {
+                authorization: Oz.client.header('http://example.com/oz/reissue', 'POST', userTicket).header
+            }
+        };
+
+        const res3 = await server.inject(req);
+        const newTicket = res3.result;
+
+        req = {
+            method: 'GET',
+            url: 'http://example.com/protected',
+            headers: {
+                authorization: Oz.client.header('http://example.com/protected', 'GET', newTicket).header
+            }
+        };
+
+        const res4 = await server.inject(req);
+        expect(res4.payload).to.equal('john your in!');
     });
 
-    it('fails to authenticate a request with mismatching app id', (done) => {
+    it('fails to authenticate a request with mismatching app id', async () => {
 
         const encryptionPassword = 'a_password_that_is_not_too_short_and_also_not_very_random_but_is_good_enough';
 
@@ -189,100 +168,79 @@ describe('Scarecrow', () => {
         const options = {
             oz: {
                 encryptionPassword,
-
-                loadAppFunc: function (id, callback) {
-
-                    callback(null, app);
-                },
-
-                loadGrantFunc: function (id, callback) {
-
-                    callback(null, grant);
-                }
+                loadAppFunc: () => app,
+                loadGrantFunc: () => ({ grant })
             }
         };
 
-        const server = new Hapi.Server();
-        server.connection();
+        const server = Hapi.server();
+        await server.register(Scarecrow);
 
-        server.register(Scarecrow, (err) => {
+        // Add strategy
 
-            expect(err).to.not.exist();
+        server.auth.strategy('oz', 'oz', options);
+        server.auth.default('oz');
 
-            // Add strategy
+        // Add a protected resource
 
-            server.auth.strategy('oz', 'oz', true, options);
+        server.route({
+            path: '/protected',
+            method: 'GET',
+            config: {
+                auth: {
+                    entity: 'user'
+                },
+                handler: function (request) {
 
-            // Add a protected resource
-
-            server.route({
-                path: '/protected',
-                method: 'GET',
-                config: {
-                    auth: {
-                        entity: 'user'
-                    },
-                    handler: function (request, reply) {
-
-                        reply(request.auth.credentials.user + ' your in!');
-                    }
+                    return request.auth.credentials.user + ' your in!';
                 }
-            });
-
-            // The app requests an app ticket using Hawk authentication
-
-            let req = {
-                method: 'POST',
-                url: 'http://example.com/oz/app',
-                headers: {
-                    authorization: Oz.client.header('http://example.com/oz/app', 'POST', app).field
-                }
-            };
-
-            server.inject(req, (res1) => {
-
-                // The user is redirected to the server, logs in, and grant app access, resulting in an rsvp
-
-                const appTicket = res1.result;
-
-                Oz.ticket.rsvp(app, grant, encryptionPassword, {}, (err, rsvp) => {
-
-                    expect(err).to.not.exist();
-
-                    // After granting app access, the user returns to the app with the rsvp
-                    // The app exchanges the rsvp for a ticket
-
-                    req = {
-                        method: 'POST',
-                        url: 'http://example.com/oz/rsvp',
-                        headers: {
-                            authorization: Oz.client.header('http://example.com/oz/rsvp', 'POST', appTicket).field
-                        },
-                        payload: JSON.stringify({ rsvp })
-                    };
-
-                    server.inject(req, (res2) => {
-
-                        const userTicket = res2.result;
-                        userTicket.app = '567';
-
-                        req = {
-                            method: 'GET',
-                            url: 'http://example.com/protected',
-                            headers: {
-                                authorization: Oz.client.header('http://example.com/protected', 'GET', userTicket).field
-                            }
-                        };
-
-                        server.inject(req, (res3) => {
-
-                            expect(res3.statusCode).to.equal(401);
-                            expect(res3.result.message).to.equal('Mismatching application id');
-                            done();
-                        });
-                    });
-                });
-            });
+            }
         });
+
+        // The app requests an app ticket using Hawk authentication
+
+        let req = {
+            method: 'POST',
+            url: 'http://example.com/oz/app',
+            headers: {
+                authorization: Oz.client.header('http://example.com/oz/app', 'POST', app).header
+            }
+        };
+
+        const res1 = await server.inject(req);
+
+        // The user is redirected to the server, logs in, and grant app access, resulting in an rsvp
+
+        const appTicket = res1.result;
+
+        const rsvp = await Oz.ticket.rsvp(app, grant, encryptionPassword);
+
+        // After granting app access, the user returns to the app with the rsvp
+        // The app exchanges the rsvp for a ticket
+
+        req = {
+            method: 'POST',
+            url: 'http://example.com/oz/rsvp',
+            headers: {
+                authorization: Oz.client.header('http://example.com/oz/rsvp', 'POST', appTicket).header
+            },
+            payload: JSON.stringify({ rsvp })
+        };
+
+        const res2 = await server.inject(req);
+        const userTicket = res2.result;
+        userTicket.app = '567';
+
+        req = {
+            method: 'GET',
+            url: 'http://example.com/protected',
+            headers: {
+                authorization: Oz.client.header('http://example.com/protected', 'GET', userTicket).header
+            }
+        };
+
+        const res3 = await server.inject(req);
+        expect(res3.statusCode).to.equal(401);
+        expect(res3.result.message).to.equal('Mismatching application id');
     });
 });
